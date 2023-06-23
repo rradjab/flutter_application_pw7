f5922139e09d226f3ce9520a679ebf8189ed1774
rradjab <rradjab@yahoo.com>
2023-06-23 17:10:55 +0400

    added as list

--- lib/fetch_file.dart
+++ lib/fetch_file.dart
@@ -11,7 +11,7 @@ class ArtistsClass {
 
 Future<List<ArtistsClass>> fetchFileFromAssets(String assetsPath) async {
   String text = await rootBundle.loadString(assetsPath).then((file) => file);
-  var data = json.decode(text);
+  var data = (json.decode(text) as List);
   return data
       .map((e) =>
           ArtistsClass(name: e['name'], link: e['link'], about: e['about']))
